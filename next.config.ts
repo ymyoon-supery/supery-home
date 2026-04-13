214cbc83862ef10aa5e85fe57205ec4ea3f607bb
ymyoon-supery <ymyoon@supery.co.kr>
2026-04-13 18:42:38 +0900

    Allow img.youtube.com for YouTube thumbnails

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,11 @@ const nextConfig: NextConfig = {
         hostname: "res.cloudinary.com",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "img.youtube.com",
+        pathname: "/**",
+      },
     ],
   },
 };
